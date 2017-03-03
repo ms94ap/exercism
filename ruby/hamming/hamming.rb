c360cc2d9f999a70201bfbd8c923e5ae679ec45f
andrew <github email address>
2017-03-03 13:10:10 +0200

    add famming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,17 @@
-class Hamming
-	def self.compute(str, str2)
-		if str == str2
-			return 0
-		elsif 
-			1
-		end
-	end
+
+module Hamming
+  extend self
+
+  VERSION = 1
+
+  def compute(stra, strb)
+    raise ArgumentError unless stra.size == strb.size
+
+    strb.chars.tap do |strb_chars|
+      return stra.chars.each.with_index.reduce(0) do |dist, (char, idx)|
+        dist += 1 if char != strb_chars[idx]
+        dist
+      end
+    end
+  end
 end
